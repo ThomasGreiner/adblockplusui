ca942b1bc0b230d68a2e9761e5d7ceeab228d756
Thomas Greiner <thomas@adblockplus.org>
2017-07-18 17:57:01 +0200

    Encapsulated script in mobile-options.js

--- mobile-options.js
+++ mobile-options.js
@@ -19,414 +19,416 @@
 
 "use strict";
 
-const {getMessage} = ext.i18n;
-
-let whitelistFilter = null;
-let promisedAcceptableAdsUrl = getAcceptableAdsUrl();
-
-/* Utility functions */
-
-function get(selector, origin)
 {
-  return (origin || document).querySelector(selector);
-}
+  const {getMessage} = ext.i18n;
 
-function getAll(selector, origin)
-{
-  return (origin || document).querySelectorAll(selector);
-}
+  let whitelistFilter = null;
+  let promisedAcceptableAdsUrl = getAcceptableAdsUrl();
 
-function create(parent, tagName, content, attributes, onclick)
-{
-  let element = document.createElement(tagName);
+  /* Utility functions */
 
-  if (typeof content == "string")
+  function get(selector, origin)
   {
-    element.textContent = content;
+    return (origin || document).querySelector(selector);
   }
 
-  if (attributes)
+  function getAll(selector, origin)
   {
-    for (let name in attributes)
-    {
-      element.setAttribute(name, attributes[name]);
-    }
+    return (origin || document).querySelectorAll(selector);
   }
 
-  if (onclick)
+  function create(parent, tagName, content, attributes, onclick)
   {
-    element.addEventListener("click", (ev) =>
+    let element = document.createElement(tagName);
+
+    if (typeof content == "string")
     {
-      onclick(ev);
-      ev.stopPropagation();
-    });
+      element.textContent = content;
+    }
+
+    if (attributes)
+    {
+      for (let name in attributes)
+      {
+        element.setAttribute(name, attributes[name]);
+      }
+    }
+
+    if (onclick)
+    {
+      element.addEventListener("click", (ev) =>
+      {
+        onclick(ev);
+        ev.stopPropagation();
+      });
+    }
+
+    parent.appendChild(element);
+    return element;
   }
 
-  parent.appendChild(element);
-  return element;
-}
+  /* Extension interactions */
 
-/* Extension interactions */
-
-function getInstalled()
-{
-  return new Promise((resolve, reject) =>
+  function getInstalled()
   {
-    ext.backgroundPage.sendMessage(
-      {type: "subscriptions.get", downloadable: true},
-      resolve
-    );
-  });
-}
-
-function getAcceptableAdsUrl()
-{
-  return new Promise((resolve, reject) =>
-  {
-    ext.backgroundPage.sendMessage(
-      {type: "prefs.get", key: "subscriptions_exceptionsurl"},
-      resolve
-    );
-  });
-}
-
-function getRecommended()
-{
-  return fetch("subscriptions.xml")
-    .then((resp) => resp.text())
-    .then((text) =>
+    return new Promise((resolve, reject) =>
     {
-      let doc = new DOMParser().parseFromString(text, "application/xml");
-      let elements = Array.from(doc.getElementsByTagName("subscription"));
-
-      return elements
-        .filter((element) => element.getAttribute("type") == "ads")
-        .map((element) =>
-        {
-          return {
-            title: element.getAttribute("title"),
-            url: element.getAttribute("url")
-          };
-        });
-    });
-}
-
-function installSubscription(url, title)
-{
-  ext.backgroundPage.sendMessage({type: "subscriptions.add", url, title});
-}
-
-function uninstallSubscription(url)
-{
-  ext.backgroundPage.sendMessage({type: "subscriptions.remove", url});
-}
-
-/* Actions */
-
-function setSubscription({disabled, title, url}, shouldAdd)
-{
-  if (disabled)
-    return;
-
-  promisedAcceptableAdsUrl.then((acceptableAdsUrl) =>
-  {
-    if (url == acceptableAdsUrl)
-    {
-      get("#acceptableAds").checked = true;
-      return;
-    }
-
-    let listInstalled = get("#subscriptions-installed");
-    let installed = get(`[data-url="${url}"]`, listInstalled);
-
-    if (installed)
-    {
-      let titleElement = get("span", installed);
-      titleElement.textContent = title || url;
-    }
-    else if (shouldAdd)
-    {
-      let element = create(listInstalled, "li", null, {"data-url": url});
-      create(element, "span", title || url);
-      create(element, "button", null, {class: "remove"},
-        () => uninstallSubscription(url)
+      ext.backgroundPage.sendMessage(
+        {type: "subscriptions.get", downloadable: true},
+        resolve
       );
+    });
+  }
+
+  function getAcceptableAdsUrl()
+  {
+    return new Promise((resolve, reject) =>
+    {
+      ext.backgroundPage.sendMessage(
+        {type: "prefs.get", key: "subscriptions_exceptionsurl"},
+        resolve
+      );
+    });
+  }
+
+  function getRecommended()
+  {
+    return fetch("subscriptions.xml")
+      .then((resp) => resp.text())
+      .then((text) =>
+      {
+        let doc = new DOMParser().parseFromString(text, "application/xml");
+        let elements = Array.from(doc.getElementsByTagName("subscription"));
+
+        return elements
+          .filter((element) => element.getAttribute("type") == "ads")
+          .map((element) =>
+          {
+            return {
+              title: element.getAttribute("title"),
+              url: element.getAttribute("url")
+            };
+          });
+      });
+  }
+
+  function installSubscription(url, title)
+  {
+    ext.backgroundPage.sendMessage({type: "subscriptions.add", url, title});
+  }
+
+  function uninstallSubscription(url)
+  {
+    ext.backgroundPage.sendMessage({type: "subscriptions.remove", url});
+  }
+
+  /* Actions */
+
+  function setSubscription({disabled, title, url}, shouldAdd)
+  {
+    if (disabled)
+      return;
+
+    promisedAcceptableAdsUrl.then((acceptableAdsUrl) =>
+    {
+      if (url == acceptableAdsUrl)
+      {
+        get("#acceptableAds").checked = true;
+        return;
+      }
+
+      let listInstalled = get("#subscriptions-installed");
+      let installed = get(`[data-url="${url}"]`, listInstalled);
+
+      if (installed)
+      {
+        let titleElement = get("span", installed);
+        titleElement.textContent = title || url;
+      }
+      else if (shouldAdd)
+      {
+        let element = create(listInstalled, "li", null, {"data-url": url});
+        create(element, "span", title || url);
+        create(element, "button", null, {class: "remove"},
+          () => uninstallSubscription(url)
+        );
+
+        let recommended = get(`#subscriptions-recommended [data-url="${url}"]`);
+        if (recommended)
+        {
+          recommended.classList.add("installed");
+        }
+      }
+    });
+  }
+
+  function removeSubscription(url)
+  {
+    promisedAcceptableAdsUrl.then((acceptableAdsUrl) =>
+    {
+      if (url == acceptableAdsUrl)
+      {
+        get("#acceptable-ads").checked = false;
+        return;
+      }
+
+      let installed = get(`#subscriptions-installed [data-url="${url}"]`);
+      if (installed)
+      {
+        installed.parentNode.removeChild(installed);
+      }
 
       let recommended = get(`#subscriptions-recommended [data-url="${url}"]`);
       if (recommended)
       {
-        recommended.classList.add("installed");
+        recommended.classList.remove("installed");
       }
-    }
-  });
-}
+    });
+  }
 
-function removeSubscription(url)
-{
-  promisedAcceptableAdsUrl.then((acceptableAdsUrl) =>
+  function setDialog(id, options)
   {
-    if (url == acceptableAdsUrl)
+    if (!id)
     {
-      get("#acceptable-ads").checked = false;
+      delete document.body.dataset.dialog;
       return;
     }
 
-    let installed = get(`#subscriptions-installed [data-url="${url}"]`);
-    if (installed)
+    let fields = getAll(`#dialog-${id} input`);
+    for (let field of fields)
     {
-      installed.parentNode.removeChild(installed);
+      field.value = (options && field.name in options) ? options[field.name] : "";
     }
+    setError(id, null);
 
-    let recommended = get(`#subscriptions-recommended [data-url="${url}"]`);
-    if (recommended)
+    document.body.dataset.dialog = id;
+  }
+
+  function setError(dialogId, message)
+  {
+    let dialog = get(`#dialog-${dialogId}`);
+    if (message)
     {
-      recommended.classList.remove("installed");
-    }
-  });
-}
-
-function setDialog(id, options)
-{
-  if (!id)
-  {
-    delete document.body.dataset.dialog;
-    return;
-  }
-
-  let fields = getAll(`#dialog-${id} input`);
-  for (let field of fields)
-  {
-    field.value = (options && field.name in options) ? options[field.name] : "";
-  }
-  setError(id, null);
-
-  document.body.dataset.dialog = id;
-}
-
-function setError(dialogId, message)
-{
-  let dialog = get(`#dialog-${dialogId}`);
-  if (message)
-  {
-    dialog.dataset.error = message;
-  }
-  else
-  {
-    delete dialog.dataset.error;
-  }
-}
-
-function populateLists()
-{
-  Promise.all([getInstalled(), getRecommended()])
-    .then(([installed, recommended]) =>
-    {
-      let listRecommended = get("#subscriptions-recommended");
-      for (let {title, url} of recommended)
-      {
-        create(listRecommended, "li", title, {"data-url": url},
-          (ev) =>
-          {
-            if (ev.target.classList.contains("installed"))
-              return;
-
-            setDialog("subscribe", {title, url});
-          }
-        );
-      }
-
-      for (let subscription of installed)
-      {
-        if (subscription.disabled)
-          continue;
-
-        setSubscription(subscription, true);
-      }
-    })
-    .catch((err) => console.error(err));
-}
-
-/* Listeners */
-
-function onChange(ev)
-{
-  if (ev.target.id != "acceptable-ads")
-    return;
-
-  promisedAcceptableAdsUrl.then((acceptableAdsUrl) =>
-  {
-    if (ev.target.checked)
-    {
-      installSubscription(acceptableAdsUrl, null);
+      dialog.dataset.error = message;
     }
     else
     {
-      uninstallSubscription(acceptableAdsUrl);
+      delete dialog.dataset.error;
     }
-  });
-}
-document.addEventListener("change", onChange);
-
-function onClick(ev)
-{
-  switch (ev.target.dataset.action)
-  {
-    case "close-dialog":
-      setDialog(null);
-      break;
-    case "open-dialog":
-      setDialog(ev.target.dataset.dialog);
-      break;
-  }
-}
-document.addEventListener("click", onClick);
-
-function onSubmit(ev)
-{
-  let fields = ev.target.elements;
-  let title = fields.title.value;
-  let url = fields.url.value;
-
-  if (!title)
-  {
-    setError("subscribe", "title");
-  }
-  else if (!url)
-  {
-    setError("subscribe", "url");
-  }
-  else
-  {
-    installSubscription(url, title);
-    setDialog(null);
   }
 
-  ev.preventDefault();
-}
-document.addEventListener("submit", onSubmit);
-
-function onToggleWhitelistFilter(ev)
-{
-  let checkbox = ev.target;
-  ext.backgroundPage.sendMessage(
-    {
-      type: (checkbox.checked) ? "filters.remove" : "filters.add",
-      text: whitelistFilter
-    }, (errors) =>
-    {
-      if (errors.length < 1)
-        return;
-
-      console.error(errors);
-      checkbox.checked = !checkbox.checked;
-    }
-  );
-  ev.preventDefault();
-}
-
-function onMessage(msg)
-{
-  switch (msg.type)
+  function populateLists()
   {
-    case "app.respond": {
-      switch (msg.action)
+    Promise.all([getInstalled(), getRecommended()])
+      .then(([installed, recommended]) =>
       {
-        case "addSubscription":
-          let [subscription] = msg.args;
-          setDialog("subscribe", {
-            title: subscription.title,
-            url: subscription.url
-          });
-          break;
-        case "showPageOptions":
-          let [{host, whitelisted}] = msg.args;
-          whitelistFilter = `@@||${host}^$document`;
+        let listRecommended = get("#subscriptions-recommended");
+        for (let {title, url} of recommended)
+        {
+          create(listRecommended, "li", title, {"data-url": url},
+            (ev) =>
+            {
+              if (ev.target.classList.contains("installed"))
+                return;
 
-          ext.i18n.setElementText(
-            get("#enabled-label"),
-            "mops_enabled_label",
-            [host]
+              setDialog("subscribe", {title, url});
+            }
           );
+        }
 
-          let checkbox = get("#enabled");
-          checkbox.checked = !whitelisted;
-          checkbox.addEventListener("click", onToggleWhitelistFilter);
-
-          get("#enabled-container").hidden = false;
-          break;
-      }
-      break;
-    }
-    case "filters.respond": {
-      let [filter] = msg.args;
-      if (!whitelistFilter || filter.text != whitelistFilter)
-        break;
-
-      get("#enabled").checked = (msg.action == "removed");
-      break;
-    }
-    case "subscriptions.respond": {
-      let [subscription] = msg.args;
-      switch (msg.action)
-      {
-        case "added":
-          setSubscription(subscription, true);
-          break;
-        case "disabled":
+        for (let subscription of installed)
+        {
           if (subscription.disabled)
-          {
-            removeSubscription(subscription.url);
-          }
-          else
-          {
-            setSubscription(subscription, true);
-          }
-          break;
-        case "removed":
-          removeSubscription(subscription.url);
-          break;
-        case "title":
-          // We're also receiving these messages for subscriptions that are not
-          // installed so we shouldn't add those by accident
-          setSubscription(subscription, false);
-          break;
+            continue;
+
+          setSubscription(subscription, true);
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+
+  /* Listeners */
+
+  function onChange(ev)
+  {
+    if (ev.target.id != "acceptable-ads")
+      return;
+
+    promisedAcceptableAdsUrl.then((acceptableAdsUrl) =>
+    {
+      if (ev.target.checked)
+      {
+        installSubscription(acceptableAdsUrl, null);
       }
-      break;
+      else
+      {
+        uninstallSubscription(acceptableAdsUrl);
+      }
+    });
+  }
+  document.addEventListener("change", onChange);
+
+  function onClick(ev)
+  {
+    switch (ev.target.dataset.action)
+    {
+      case "close-dialog":
+        setDialog(null);
+        break;
+      case "open-dialog":
+        setDialog(ev.target.dataset.dialog);
+        break;
     }
   }
+  document.addEventListener("click", onClick);
+
+  function onSubmit(ev)
+  {
+    let fields = ev.target.elements;
+    let title = fields.title.value;
+    let url = fields.url.value;
+
+    if (!title)
+    {
+      setError("subscribe", "title");
+    }
+    else if (!url)
+    {
+      setError("subscribe", "url");
+    }
+    else
+    {
+      installSubscription(url, title);
+      setDialog(null);
+    }
+
+    ev.preventDefault();
+  }
+  document.addEventListener("submit", onSubmit);
+
+  function onToggleWhitelistFilter(ev)
+  {
+    let checkbox = ev.target;
+    ext.backgroundPage.sendMessage(
+      {
+        type: (checkbox.checked) ? "filters.remove" : "filters.add",
+        text: whitelistFilter
+      }, (errors) =>
+      {
+        if (errors.length < 1)
+          return;
+
+        console.error(errors);
+        checkbox.checked = !checkbox.checked;
+      }
+    );
+    ev.preventDefault();
+  }
+
+  function onMessage(msg)
+  {
+    switch (msg.type)
+    {
+      case "app.respond": {
+        switch (msg.action)
+        {
+          case "addSubscription":
+            let [subscription] = msg.args;
+            setDialog("subscribe", {
+              title: subscription.title,
+              url: subscription.url
+            });
+            break;
+          case "showPageOptions":
+            let [{host, whitelisted}] = msg.args;
+            whitelistFilter = `@@||${host}^$document`;
+
+            ext.i18n.setElementText(
+              get("#enabled-label"),
+              "mops_enabled_label",
+              [host]
+            );
+
+            let checkbox = get("#enabled");
+            checkbox.checked = !whitelisted;
+            checkbox.addEventListener("click", onToggleWhitelistFilter);
+
+            get("#enabled-container").hidden = false;
+            break;
+        }
+        break;
+      }
+      case "filters.respond": {
+        let [filter] = msg.args;
+        if (!whitelistFilter || filter.text != whitelistFilter)
+          break;
+
+        get("#enabled").checked = (msg.action == "removed");
+        break;
+      }
+      case "subscriptions.respond": {
+        let [subscription] = msg.args;
+        switch (msg.action)
+        {
+          case "added":
+            setSubscription(subscription, true);
+            break;
+          case "disabled":
+            if (subscription.disabled)
+            {
+              removeSubscription(subscription.url);
+            }
+            else
+            {
+              setSubscription(subscription, true);
+            }
+            break;
+          case "removed":
+            removeSubscription(subscription.url);
+            break;
+          case "title":
+            // We're also receiving these messages for subscriptions that are not
+            // installed so we shouldn't add those by accident
+            setSubscription(subscription, false);
+            break;
+        }
+        break;
+      }
+    }
+  }
+  ext.onMessage.addListener(onMessage);
+
+  ext.backgroundPage.sendMessage({
+    type: "app.listen",
+    filter: ["addSubscription", "showPageOptions"]
+  });
+
+  ext.backgroundPage.sendMessage({
+    type: "filters.listen",
+    filter: ["added", "removed"]
+  });
+
+  ext.backgroundPage.sendMessage({
+    type: "subscriptions.listen",
+    filter: ["added", "disabled", "removed", "title"]
+  });
+
+  /* Initialization */
+
+  populateLists();
+
+  getDocLink("acceptable_ads", (link) =>
+  {
+    get("#acceptableAds-more").href = link;
+  });
+
+  get("#dialog-subscribe [name='title']").setAttribute(
+    "placeholder",
+    getMessage("mops_subscribe_title")
+  );
+
+  get("#dialog-subscribe [name='url']").setAttribute(
+    "placeholder",
+    getMessage("mops_subscribe_url")
+  );
 }
-ext.onMessage.addListener(onMessage);
-
-ext.backgroundPage.sendMessage({
-  type: "app.listen",
-  filter: ["addSubscription", "showPageOptions"]
-});
-
-ext.backgroundPage.sendMessage({
-  type: "filters.listen",
-  filter: ["added", "removed"]
-});
-
-ext.backgroundPage.sendMessage({
-  type: "subscriptions.listen",
-  filter: ["added", "disabled", "removed", "title"]
-});
-
-/* Initialization */
-
-populateLists();
-
-getDocLink("acceptable_ads", (link) =>
-{
-  get("#acceptableAds-more").href = link;
-});
-
-get("#dialog-subscribe [name='title']").setAttribute(
-  "placeholder",
-  getMessage("mops_subscribe_title")
-);
-
-get("#dialog-subscribe [name='url']").setAttribute(
-  "placeholder",
-  getMessage("mops_subscribe_url")
-);
